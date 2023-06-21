28adacdb53aea7fc5180ff1846d5d8467986d2e8
Bobby Krupczak <rdk@krupczak.org>
2023-06-21 10:06:53 -0400

    Initial commit of working Swift EGM96 offset calculator based on work by https://github.com/matthiaszimmermann.  Ported to Swift/iOS using ChatGPT-4 plus lots of tweaks and fixes for things that ChatGPT-4 broke.  Few enhancements.  Plus switched to zlib compression since it is supported by iOS.

---
+++ zuncompress.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import zlib
+import argparse
+import os
+import sys
+
+# Define command-line arguments
+parser = argparse.ArgumentParser(description='Uncompress a file using zlib')
+
+parser.add_argument('infile', help='Input file to be uncompressed')
+parser.add_argument('outfile', help='Output file to store uncompressed data')
+
+# Check if no arguments were provided
+if len(sys.argv)==1:
+    parser.print_help(sys.stderr)
+    sys.exit(1)
+
+args = parser.parse_args()
+
+# Check if input file exists
+if not os.path.isfile(args.infile):
+    print(f"Input file {args.infile} does not exist.")
+    exit(1)
+
+# Check if output file already exists
+if os.path.isfile(args.outfile):
+    print(f"Output file {args.outfile} already exists.")
+    exit(1)
+
+# Read the file
+with open(args.infile, 'rb') as f_in:
+    data = f_in.read()
+
+# Compress the file
+uncompressed_data = zlib.decompress(data)
+
+# Write the compressed file
+with open(args.outfile, 'wb') as f_out:
+    f_out.write(uncompressed_data)
